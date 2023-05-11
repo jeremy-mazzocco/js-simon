43abc04c431799a9d52eb0b715257330b4c9caf0
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-11 19:57:34 +0100

    refactoring

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-//crea una array con 5 numeri
+// Variabili
 const numeriDaIndoviare = [];
-const selectConatiner = document.getElementById('container');
 const numeriUtente = [];
-let numeriChiesti;
+const lunghezzaArray = 5;
+const selectConatiner = document.getElementById('container');
 
-while (numeriDaIndoviare.length < 5) {
-    const creaNumeroRandom = randomNumber(1, 100);
-    if (!numeriDaIndoviare.includes(creaNumeroRandom)) {
-        numeriDaIndoviare.push(creaNumeroRandom);
-    }
-}
+
+//Crea una array con 5 numeri
+insertNumberInArray(numeriDaIndoviare, lunghezzaArray);
 console.log(numeriDaIndoviare);
 
 // Scrivi i 5 numeri nell'HTML
@@ -18,28 +15,19 @@ selectConatiner.innerHTML += numeriDaIndoviare;
 // dopo 30sec scompaiono i numeri da indovinare
 setTimeout(() => {
     selectConatiner.innerHTML = " ";
-}, 30000);
+}, 5000);
 
-// chiedi numeri all'utente per 5 volte
+// chiedi numeri all'utente per 5 volte e mettili in una array
 setTimeout(() => {
-    while (numeriUtente.length < numeriDaIndoviare.length) {
-        numeriChiesti = parseInt(prompt("Inserisci un numero"));
-        numeriUtente.push(numeriChiesti);
+    while (numeriUtente.length <= numeriDaIndoviare.length) {
+        let richiediNumeri = parseInt(prompt("Inserisci un numero"));
+        numeriUtente.push(richiediNumeri);
     }
     console.log(numeriUtente);
 
-    // controlla se i 5 numeri combaciano con quelli dell'utent o mostrati
-    for (let i = 0; i < numeriDaIndoviare.length; i++) {
-        if (numeriDaIndoviare.includes(numeriUtente[i])) {
-            console.log("numero trovato", numeriUtente[i]);
-        }
-    }
-}, 31000);
-
-
-
-
-
+    // controlla se i numeri da indovinare combaciano con quelli dell'utente
+    checkArryWithArray(numeriDaIndoviare, numeriUtente);
+}, 5100);
 
 
 
@@ -52,10 +40,21 @@ function randomNumber(min, max) {
     return random
 }
 
-// Metti dentro una array una varialibe e controlla che non ci sia gia'
-// while (numeriDaIndoviare.length < 5) {
-//     const creaNumeroRandom = randomNumber(1, 100);
-//     if (!numeriDaIndoviare.includes(creaNumeroRandom)) {
-//         numeriDaIndoviare.push(creaNumeroRandom);
-//     }
-// }
+// Controlla una array con un altra
+function checkArryWithArray(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1.includes(arr2[i])) {
+            console.log("numero trovato:", arr2[i]);
+        }
+    }
+}
+
+// Metti dentro una array tot numeri
+function insertNumberInArray(arr, lunghezza) {
+    while (arr.length < lunghezza) {
+        const creaNumeroRandom = randomNumber(1, 100);
+        if (!arr.includes(creaNumeroRandom)) {
+            arr.push(creaNumeroRandom);
+        }
+    }
+}
